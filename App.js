70bafa573ba4cca9244146335d99cba69fb55fd5
shriyanspur <65410272+shriyanspur@users.noreply.github.com>
2021-02-10 21:57:40 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,24 @@
+import * as React from 'react';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import WelcomeScreen from './screens/WelcomeScreen';
+import { createAppContainer, createSwitchNavigator } from 'react-navigation';
+import { AppTabNavigator } from './components/AppTabNavigator';
+import { AppDrawerNavigator } from './components/AppDrawerNavigator';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <SafeAreaProvider>
+        <AppContainer />
+      </SafeAreaProvider>
+    )
+  }
+}
+
+const switchNavigator = createSwitchNavigator({
+  welcomeScreen: {screen: WelcomeScreen},
+  drawer: {screen: AppDrawerNavigator}
+})
+
+const AppContainer = createAppContainer(switchNavigator);
